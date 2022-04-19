baa2eb55fb414639ab4ec5fbdd9e480ae05b5990
luc siat <lucsiat@gmail.com>
2022-04-19 19:36:05 +0200

    created the migration

---
+++ app/models/restaurant.rb
@@ -0,0 +1,6 @@
+class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+  categories = %w[chinese italian japanese french belgian]
+  validates :name, :address, :category, presence: true
+  validates :category, inclusion: { in: categories }
+end
